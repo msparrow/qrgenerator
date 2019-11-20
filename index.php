a6b10186504d1095d5bbc568a5870fe43b38e1a2
msparrow <msparrow@ucsd.edu>
2019-11-20 10:52:15 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,29 +5,37 @@
   <!-- Import of JQuery and JS Script for generating QR Codes -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
-  
+  <script src="js/qrgenerator.js"></script>
 <style>
   #qrcode {
   width:240px;
-  height:180px;
+  height:200px;
   margin-top:20px;
-  margin-bottom:50px;
-  margin-right:auto;
   margin-left:auto;
+  margin-right:auto;
+  position:relative;
+  /*zoom:200%;*/
 }
   input{
-  text-align:center;
+    /*zoom:200%;*/
+    text-align:center;
   }
   body{
-   zoom:200%; 
+    zoom:200%;
   }
 </style>
 </head>
-  
 <body>
   <!-- Text input which will be converted into a QR Code-->
-  <input id="text" type="text" value="Hello World" style="width:100%" /><br />
+  <div id="body">
+  <input id="text" type="text" value="Hello World" style="width:100%"></input> 
+  <br />
   <div id="qrcode"></div>
-  <script src="./js/qrgenerator.js"></script>
+  <br />
+  <br />
+  <br />
+  <br />
+  <input id="submit" type="button" value="Request QR Data"></input>
+  </div>
 </body>
 </html>
